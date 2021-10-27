ebf28b9b080dc015e5903b59245cacddffefeed7
GMMarshall <gmmarshall27@gmail.com>
2021-10-27 14:46:13 -0400

    initial commit with handle bars

---
+++ public/js/login.js
@@ -0,0 +1,26 @@
+const loginForm = async function(event) {
+    event.preventDefault();
+
+    const username = document.querySelector('#username-input-login');
+    const password = document.querySelector('#password-input-login');
+
+    const response = await fetch('/api/user/login', {
+        method: 'POST',
+        body: JSON.stringify({
+          username: usernameEl.value,
+          password: passwordEl.value,
+        }),
+        headers: { 'Content-Type': 'application/json' },
+      });
+    
+      if (response.ok) {
+        document.location.replace('/dashboard');
+      } else {
+        alert('Failed to login');
+      }
+    };
+    
+    document
+      .querySelector('#login-form')
+      .addEventListener('submit', loginForm);
+    
