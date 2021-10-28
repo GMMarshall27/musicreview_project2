a885044275b9dd6d3fcf51b41a9e3830d602c851
Adedotun O. Albert-Ojeikere <dotun1m@gmail.com>
2021-10-28 14:26:39 -0400

    route made but will change later

--- controllers/api/comment-routes.js
+++ controllers/api/comment-routes.js
@@ -1,5 +1,17 @@
 const router = require('express').Router();
 const { Comment } = require('../../models/');
 
+//got from homework will change
+router.post('/', withAuth, async (req, res) => {
+    try {
+      const commentNew = await Comment.create({
+        ...req.body,
+        userId: req.session.userId,
+      });
+      res.json(commentNew);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
 
 module.exports = router;
