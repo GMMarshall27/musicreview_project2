acd41adb407f93a16dd8b3167473e9eb1aee7d24
Donovan K <7donovan7@gmail.com>
2021-10-29 02:09:56 -0400

    handlebars for signup

--- server.js
+++ server.js
@@ -33,6 +33,10 @@ app.get('/login', (req, res) => {
     res.render('login');
 });
 
+app.get('/signup', (req, res) => {
+    res.render('signup');
+});
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(express.json());
