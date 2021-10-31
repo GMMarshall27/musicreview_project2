56a5f2dcd21ba81becc935f1909f79a51be82032
Donovan K <7donovan7@gmail.com>
2021-10-31 15:40:00 -0400

    revamping log

--- server.js
+++ server.js
@@ -39,10 +39,6 @@ app.get('/login', (req, res) => {
 	res.render('login');
 });
 
-app.get("/login", (req, res) => {
-  res.render("login");
-});
-
 app.get("/signup", (req, res) => {
   res.render("signup");
 });
